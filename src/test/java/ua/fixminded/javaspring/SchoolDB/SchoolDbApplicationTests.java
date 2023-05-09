d19048854afc8119460b9423eeceffd87a8c6b27
”Kyrylo <kirillartemov1993@gmail.com>
2023-05-09 14:51:18 +0200

    Initiail commit

---
+++ src/test/java/ua/fixminded/javaspring/SchoolDB/SchoolDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.fixminded.javaspring.SchoolDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
