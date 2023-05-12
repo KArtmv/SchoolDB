ae02391ba77634445caa47eaa01a08ebc1af0ccd
”Kyrylo <kirillartemov1993@gmail.com>
2023-05-12 12:07:13 +0200

    created main class

--- src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
@@ -1,4 +1,4 @@
-package ua.fixminded.javaspring.SchoolDB;
+package ua.foxminded.javaspring.schooldb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
