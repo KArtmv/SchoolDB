0bee665213f8ae7d9173f5c28874eed47b7b108c
”Kyrylo <kirillartemov1993@gmail.com>
2023-05-12 12:09:42 +0200

    class of configuration of spring

---
+++ src/main/java/ua/foxminded/javaspring/schooldb/SpringJdbcConfig.java
@@ -0,0 +1,24 @@
+package ua.foxminded.javaspring.schooldb;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan()
+public class SpringJdbcConfig {
+	@Bean
+	public DataSource postDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/schoolDB");
+		dataSource.setUsername("admin");
+		dataSource.setPassword("admin");
+		dataSource.setSchema("public");
+		return dataSource;
+
+	}
+}
