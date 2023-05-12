367b7a3e303bf5d8eaf2b71bc122b94d559405f0
”Kyrylo <kirillartemov1993@gmail.com>
2023-05-12 12:10:57 +0200

    scripts of create tables and insert data of flyway

---
+++ src/main/resources/db/migration/V6__insert_data_into_group.sql
@@ -0,0 +1,11 @@
+insert into groups (group_name) values ('YS-27');
+insert into groups (group_name) values ('HV-14');
+insert into groups (group_name) values ('QM-09');
+insert into groups (group_name) values ('ZF-83');
+insert into groups (group_name) values ('RG-52');
+insert into groups (group_name) values ('PL-41');
+insert into groups (group_name) values ('KX-76');
+insert into groups (group_name) values ('ND-98');
+insert into groups (group_name) values ('TC-63');
+insert into groups (group_name) values ('WU-48');
+insert into groups (group_name) values ('Students without group');
