93625e786aee87e907fe87c741b5538f72899f62
”Kyrylo <kirillartemov1993@gmail.com>
2023-05-15 09:30:58 +0200

    added AnnotationConfigApplicationContext and start app

--- src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
@@ -2,12 +2,16 @@ package ua.foxminded.javaspring.schooldb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SchoolDbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolDbApplication.class, args);
-	}
 
+		AnnotationConfigApplicationContext contex = new AnnotationConfigApplicationContext(SpringJdbcConfig.class);
+		Starter starter = new Starter(contex);
+		starter.appStart();
+	}
 }
