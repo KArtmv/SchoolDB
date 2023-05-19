d09ad475db66136a852aadfab3148a4bcbb549c0
”Kyrylo <kirillartemov1993@gmail.com>
2023-05-19 11:07:49 +0200

    countStudentAtGrops now return list of CountStudentsAtGroup instead list of map

--- src/main/java/ua/foxminded/javaspring/schooldb/dao/GroupDAO.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/dao/GroupDAO.java
@@ -1,13 +1,12 @@
 package ua.foxminded.javaspring.schooldb.dao;
 
 import java.util.List;
-import java.util.Map;
-
+import ua.foxminded.javaspring.schooldb.model.CountStudentsAtGroup;
 import ua.foxminded.javaspring.schooldb.model.Group;
 
 public interface GroupDAO {
 
 	List<Group> listOfGroups();
 
-	List<Map<String, Object>> counterStudentsAtGroups(int count);
+	List<CountStudentsAtGroup> counterStudentsAtGroups(int count);
 }
