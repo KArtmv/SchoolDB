e2c1ece563de140c7685d6764936809089adbdd1
”Kyrylo <kirillartemov1993@gmail.com>
2023-05-19 11:17:05 +0200

    added springJDBCConfig which returns a DataSource for FlywayMigration which is also added

--- src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
@@ -1,16 +1,26 @@
 package ua.foxminded.javaspring.schooldb;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import ua.foxminded.javaspring.schooldb.FlywayMigration.FlywayMigration;
+import ua.foxminded.javaspring.schooldb.config.SpringJdbcConfig;
 
 @SpringBootApplication
 public class SchoolDbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolDbApplication.class, args);
+		SpringJdbcConfig jdbcConfig = new SpringJdbcConfig();
 
 		AnnotationConfigApplicationContext contex = new AnnotationConfigApplicationContext(SpringJdbcConfig.class);
+		DataSource dataSource = jdbcConfig.postDataSource();
+
+		FlywayMigration migration = new FlywayMigration(dataSource);
+		migration.flywayMigration();
 		Starter starter = new Starter(contex);
 		starter.appStart();
 	}
