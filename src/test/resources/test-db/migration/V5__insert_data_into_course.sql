eaa48a1ca93e1983338d3cbbf5efeca92062d935
”Kyrylo <kirillartemov1993@gmail.com>
2023-06-20 19:32:45 +0200

    flyway migrate scripts of tests

---
+++ src/test/resources/test-db/migration/V5__insert_data_into_course.sql
@@ -0,0 +1,10 @@
+insert into courses (course_name, course_description) values ('Principles of Economics', 'Learn about the fundamentals of economics.');
+insert into courses (course_name, course_description) values ('World History: Ancient Civilizations', 'Explore the history of ancient civilizations.');
+insert into courses (course_name, course_description) values ('Creative Writing Workshop', 'Develop your writing skills in a creative environment.');
+insert into courses (course_name, course_description) values ('Introduction to Psychology', 'Learn the basics of psychology.');
+insert into courses (course_name, course_description) values ('Business Ethics and Social Responsibility', 'Explore ethical issues in business and the role of social responsibility.');
+insert into courses (course_name, course_description) values ('Human Anatomy and Physiology', 'Study the human body and its functions.');
+insert into courses (course_name, course_description) values ('Principles of Marketing', 'Learn the basics of marketing.');
+insert into courses (course_name, course_description) values ('Environmental Science and Sustainability', 'Study the science of the environment and explore issues of sustainability.');
+insert into courses (course_name, course_description) values ('Fundamentals of Graphic Design', 'Learn the basics of graphic design.');
+insert into courses (course_name, course_description) values ('Introduction to Computer Science', 'An introduction to computer science principles and programming.');
