4dd5d29b282da8f4957061a2ac03b54481123758
”Kyrylo <kirillartemov1993@gmail.com>
2023-06-20 19:34:34 +0200

    Configurable context corrected

--- src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
@@ -4,24 +4,21 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+import org.springframework.context.ConfigurableApplicationContext;
 import ua.foxminded.javaspring.schooldb.FlywayMigration.FlywayMigration;
-import ua.foxminded.javaspring.schooldb.config.SpringJdbcConfig;
 
 @SpringBootApplication
 public class SchoolDbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchoolDbApplication.class, args);
-		SpringJdbcConfig jdbcConfig = new SpringJdbcConfig();
+		ConfigurableApplicationContext contex = SpringApplication.run(SchoolDbApplication.class, args);
 
-		AnnotationConfigApplicationContext contex = new AnnotationConfigApplicationContext(SpringJdbcConfig.class);
-		DataSource dataSource = jdbcConfig.postDataSource();
+		DataSource dataSource = contex.getBean(DataSource.class);
 
 		FlywayMigration migration = new FlywayMigration(dataSource);
 		migration.flywayMigration();
-		Starter starter = new Starter(contex);
+
+		Starter starter = contex.getBean(Starter.class);
 		starter.appStart();
 	}
 }
