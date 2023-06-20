4bf072cee63eb45c5a1872e14b5487eb23e17c58
”Kyrylo <kirillartemov1993@gmail.com>
2023-06-20 18:36:54 +0200

    removed empty line

--- src/main/java/ua/foxminded/javaspring/schooldb/rowmapper/CourseMapper.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/rowmapper/CourseMapper.java
@@ -11,10 +11,9 @@ public class CourseMapper implements RowMapper<Course> {
 
 	@Override
 	public Course mapRow(ResultSet rs, int rowNum) throws SQLException {
-
-		return new Course(rs.getLong("course_id"),
+		return new Course(
+				rs.getLong("course_id"),
 				rs.getString("course_name"),
 				rs.getString("course_description"));
 	}
-
 }
