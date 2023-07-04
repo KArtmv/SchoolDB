2446f2c2f71ac822549babc246c8c78572dfef1f
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-04 12:26:09 +0200

    add annotation @component
    corrected some values name

--- src/main/java/ua/foxminded/javaspring/schooldb/option/DeleteStudent.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/option/DeleteStudent.java
@@ -1,34 +1,37 @@
 package ua.foxminded.javaspring.schooldb.option;
 
+import org.springframework.stereotype.Component;
+
 import ua.foxminded.javaspring.schooldb.dao.CourseDAO;
 import ua.foxminded.javaspring.schooldb.dao.StudentDAO;
-import ua.foxminded.javaspring.schooldb.input.InputStudentId;
+import ua.foxminded.javaspring.schooldb.input.InputStudentID;
 import ua.foxminded.javaspring.schooldb.model.Student;
 
+@Component
 public class DeleteStudent {
 
 	private final StudentDAO studentDAO;
-	private final InputStudentId inputStudentId;
+	private final InputStudentID inputStudentID;
 	private final CourseDAO courseDAO;
 
-	public DeleteStudent(StudentDAO studentDAO, InputStudentId inputStudentId, CourseDAO courseDAO) {
+	public DeleteStudent(StudentDAO studentDAO, InputStudentID inputStudentID, CourseDAO courseDAO) {
 		this.studentDAO = studentDAO;
-		this.inputStudentId = inputStudentId;
+		this.inputStudentID = inputStudentID;
 		this.courseDAO = courseDAO;
 	}
 
 	public void deleteStudent() {
 		System.out.println("Enter the ID of the student to be removed");
 
-		Student inputId = inputStudentId.inputStudentId();
+		Student inputID = inputStudentID.inputID();
 
-		boolean deleteFromCourses = courseDAO.deleteStudentFromTheirAllCourses(inputId);
+		boolean deleteFromCourses = courseDAO.deleteStudentFromTheirAllCourses(inputID);
 		if (deleteFromCourses) {
-			System.out.println("student deleted from the all their courses");
+			System.out.println("student is deleted from the all their courses");
 
-			boolean deleted = studentDAO.deleteStudent(inputId);
+			boolean deleted = studentDAO.deleteStudent(inputID);
 			if (deleted) {
-				System.out.println("student deleted");
+				System.out.println("Student is deleted");
 			}
 		}
 	}
