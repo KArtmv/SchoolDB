67846164cb4f522da1df6b1fe502e56153722898
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-04 13:02:24 +0200

    removed not needed lines

--- src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/SchoolDbApplication.java
@@ -1,24 +1,12 @@
 package ua.foxminded.javaspring.schooldb;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ua.foxminded.javaspring.schooldb.FlywayMigration.FlywayMigration;
 
 @SpringBootApplication
 public class SchoolDbApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext contex = SpringApplication.run(SchoolDbApplication.class, args);
-
-		DataSource dataSource = contex.getBean(DataSource.class);
-
-		FlywayMigration migration = new FlywayMigration(dataSource);
-		migration.flywayMigration();
-
-		Starter starter = contex.getBean(Starter.class);
-		starter.appStart();
+		SpringApplication.run(SchoolDbApplication.class, args);
 	}
 }
