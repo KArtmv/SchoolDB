45e2f0efe8e39d5d5de109ff0a704423c16e53f7
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-04 13:01:52 +0200

    removed lines of construction injection
    instead it use dependency injection of spring boot.
    moved switches from switchOfMenu.class
    appStart method now marked as @postConstruction

--- src/main/java/ua/foxminded/javaspring/schooldb/Starter.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/Starter.java
@@ -2,68 +2,88 @@ package ua.foxminded.javaspring.schooldb;
 
 import java.util.Scanner;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import ua.foxminded.javaspring.schooldb.dao.CourseDAO;
-import ua.foxminded.javaspring.schooldb.dao.GroupDAO;
-import ua.foxminded.javaspring.schooldb.dao.StudentDAO;
-import ua.foxminded.javaspring.schooldb.input.InputData;
-import ua.foxminded.javaspring.schooldb.input.InputDataOfStudent;
-import ua.foxminded.javaspring.schooldb.input.InputNumbers;
-import ua.foxminded.javaspring.schooldb.input.InputStudentId;
 import ua.foxminded.javaspring.schooldb.option.AddStudent;
 import ua.foxminded.javaspring.schooldb.option.AddStudentToCourse;
 import ua.foxminded.javaspring.schooldb.option.AllStudentFromCourse;
 import ua.foxminded.javaspring.schooldb.option.DeleteStudent;
 import ua.foxminded.javaspring.schooldb.option.DeleteStudentFromCourse;
-import ua.foxminded.javaspring.schooldb.option.StudentCountGroupFinder;
-import ua.foxminded.javaspring.schooldb.output.ShowListOfCourses;
-import ua.foxminded.javaspring.schooldb.output.ShowListOfCoursesOfStudent;
-import ua.foxminded.javaspring.schooldb.output.ShowListOfGroups;
+import ua.foxminded.javaspring.schooldb.option.StudentCountChecker;
 
 @Component
+@Profile("!test")
 public class Starter {
-	private final AnnotationConfigApplicationContext contex;
 
-	@Autowired
-	public Starter(AnnotationConfigApplicationContext contex) {
-		this.contex = contex;
+	private final Scanner scanner;
+	private final StudentCountChecker countGroupFinder;
+	private final AllStudentFromCourse studentFromCourse;
+	private final AddStudent addStudent;
+	private final DeleteStudent deleteStudent;
+	private final AddStudentToCourse studentToCourse;
+	private final DeleteStudentFromCourse deleteStudentFromCourse;
+	private final StartMenu startMenu;
+
+	public Starter(Scanner scanner, StudentCountChecker countGroupFinder, AllStudentFromCourse studentFromCourse,
+			AddStudent addStudent, DeleteStudent deleteStudent, AddStudentToCourse studentToCourse,
+			DeleteStudentFromCourse deleteStudentFromCourse, StartMenu startMenu) {
+		this.scanner = scanner;
+		this.countGroupFinder = countGroupFinder;
+		this.studentFromCourse = studentFromCourse;
+		this.addStudent = addStudent;
+		this.deleteStudent = deleteStudent;
+		this.studentToCourse = studentToCourse;
+		this.deleteStudentFromCourse = deleteStudentFromCourse;
+		this.startMenu = startMenu;
 	}
 
+	@PostConstruct
 	public void appStart() {
-		StudentDAO studentDAO = contex.getBean(StudentDAO.class);
-		GroupDAO groupDAO = contex.getBean(GroupDAO.class);
-		CourseDAO courseDAO = contex.getBean(CourseDAO.class);
-
-		Scanner scanner = new Scanner(System.in);
-		InputNumbers numbers = new InputNumbers(scanner);
-		InputData inputData = new InputData(scanner);
-		InputStudentId inputStudentId = new InputStudentId(studentDAO, numbers);
-
-		ShowListOfCoursesOfStudent coursesOfStudent = new ShowListOfCoursesOfStudent();
-		ShowListOfCourses listOfCourses = new ShowListOfCourses(courseDAO);
-		ShowListOfGroups listOfGroups = new ShowListOfGroups(groupDAO);
-
-		StudentCountGroupFinder countGroupFinder = new StudentCountGroupFinder(groupDAO, numbers);
-		DeleteStudent deleteStudent = new DeleteStudent(studentDAO, inputStudentId, courseDAO);
-		AllStudentFromCourse studentFromCourse = new AllStudentFromCourse(courseDAO, listOfCourses, numbers);
-		DeleteStudentFromCourse deleteStudentFromCourse = new DeleteStudentFromCourse(courseDAO, studentDAO,
-				inputStudentId, numbers, coursesOfStudent);
-		AddStudentToCourse studentToCourse = new AddStudentToCourse(courseDAO, listOfCourses, inputStudentId, numbers);
-		InputDataOfStudent dataOfStudent = new InputDataOfStudent(listOfGroups, scanner, inputData, numbers);
-		AddStudent addStudent = new AddStudent(studentDAO, dataOfStudent);
-
-		SwitchOfMenu switchOfMenu = new SwitchOfMenu(scanner, countGroupFinder, studentFromCourse, addStudent,
-				deleteStudent, studentToCourse, deleteStudentFromCourse);
-
 		boolean exit = false;
-		StartMenu startMenu = new StartMenu();
 		startMenu.startMenu();
 
 		while (!exit) {
-			exit = switchOfMenu.switchOfMenu(exit, startMenu);
+			try {
+				switch (scanner.nextLine()) {
+				case "1": {
+					countGroupFinder.checkByCount();
+					break;
+				}
+				case "2": {
+					studentFromCourse.findAllStudentsAtCourse();
+					break;
+				}
+				case "3": {
+					addStudent.addStudent();
+					break;
+				}
+				case "4": {
+					deleteStudent.deleteStudent();
+					break;
+				}
+				case "5": {
+					studentToCourse.addStudentToCourse();
+					break;
+				}
+				case "6": {
+					deleteStudentFromCourse.deleteFromCourse();
+					break;
+				}
+				case "x": {
+					exit = true;
+					break;
+				}
+				default:
+					startMenu.startMenu();
+					System.out.println("Enter number of option you want");
+					break;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
