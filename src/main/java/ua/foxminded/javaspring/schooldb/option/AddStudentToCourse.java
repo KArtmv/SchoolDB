5c4782b285037afc0cc86db301a637403b5417c9
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-04 12:13:50 +0200

    add annotation @component
    corrected some values name

--- src/main/java/ua/foxminded/javaspring/schooldb/option/AddStudentToCourse.java
+++ src/main/java/ua/foxminded/javaspring/schooldb/option/AddStudentToCourse.java
@@ -1,37 +1,41 @@
 package ua.foxminded.javaspring.schooldb.option;
 
+import org.springframework.stereotype.Component;
+
 import ua.foxminded.javaspring.schooldb.dao.CourseDAO;
 import ua.foxminded.javaspring.schooldb.input.InputNumbers;
-import ua.foxminded.javaspring.schooldb.input.InputStudentId;
+import ua.foxminded.javaspring.schooldb.input.InputStudentID;
 import ua.foxminded.javaspring.schooldb.model.Course;
 import ua.foxminded.javaspring.schooldb.model.Student;
-import ua.foxminded.javaspring.schooldb.output.ShowListOfCourses;
+import ua.foxminded.javaspring.schooldb.output.ShowCoursesList;
 
+@Component
 public class AddStudentToCourse {
 
 	private final CourseDAO сourseDAO;
-	private final ShowListOfCourses listOfCourses;
-	private final InputStudentId insertStudentId;
-	private final InputNumbers inputNumbers;
+	private final ShowCoursesList coursesList;
+	private final InputStudentID inputStudentID;
+	private final InputNumbers number;
 
-	public AddStudentToCourse(CourseDAO сourseDAO, ShowListOfCourses listOfCourses, InputStudentId insertStudentId,
-			InputNumbers inputNumbers) {
+	public AddStudentToCourse(CourseDAO сourseDAO, ShowCoursesList coursesList, InputStudentID inputStudentID,
+			InputNumbers number) {
 		this.сourseDAO = сourseDAO;
-		this.listOfCourses = listOfCourses;
-		this.insertStudentId = insertStudentId;
-		this.inputNumbers = inputNumbers;
+		this.coursesList = coursesList;
+		this.inputStudentID = inputStudentID;
+		this.number = number;
 	}
 
 	public void addStudentToCourse() {
-		System.out.println("Enter the ID of the student you want to add to the course");
-		Student studentId = insertStudentId.inputStudentId();
+		System.out.println("Enter the student ID you want to add to the course");
+		Student studentID = inputStudentID.inputID();
 
-		listOfCourses.showListofCourses();
+		coursesList.coursesList();
 
-		System.out.println("select the course you want to add the student to");
-		int courseId = inputNumbers.inputNumbers();
+		System.out.println("Select the course you want to add the student.");
 
-		boolean added = сourseDAO.addStudentAtCourse(studentId, new Course(courseId));
+		Course inputCourseID = new Course(Long.valueOf(number.input()));
+
+		boolean added = сourseDAO.addStudentToCourse(studentID, inputCourseID);
 
 		if (added) {
 			System.out.println("Student have been added at course");
