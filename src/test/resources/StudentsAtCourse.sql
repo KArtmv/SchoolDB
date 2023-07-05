7f68973231047ac967882382f641175db057f18a
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-05 13:44:53 +0200

    tests resources files

---
+++ src/test/resources/StudentsAtCourse.sql
@@ -0,0 +1,5 @@
+insert into studenttocourse (student_id, course_id) values (1, 1);
+insert into studenttocourse (student_id, course_id) values (2, 1);
+insert into studenttocourse (student_id, course_id) values (3, 1);
+insert into studenttocourse (student_id, course_id) values (4, 1);
+insert into studenttocourse (student_id, course_id) values (5, 1);
