3a2745112cadf26e3a1ef8837c66b9efc47c6cf1
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-07 16:40:00 +0200

    the test has been separated to several smaller tests

--- src/test/java/ua/foxminded/javaspring/schooldb/input/InputStudentDataTest.java
+++ src/test/java/ua/foxminded/javaspring/schooldb/input/InputStudentDataTest.java
@@ -3,9 +3,6 @@ package ua.foxminded.javaspring.schooldb.input;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -16,7 +13,23 @@ import ua.foxminded.javaspring.schooldb.output.ShowGroupsList;
 public class InputStudentDataTest {
 
 	@Test
-	void checkInputDataOfStudentTest() {
+	void shouldReturnNonNullDataOfStudent() {
+		Student result = inputDataOfStudent();
+
+		assertNotNull(result);
+	}
+
+	@Test
+	void shouldReturnValidDataOfStudent() {
+		Student result = inputDataOfStudent();
+
+		assertEquals("test", result.getFirstName());
+		assertEquals("test", result.getLastName());
+		assertEquals(1L, result.getGroupID());
+		assertNull(result.getStudentID());
+	}
+
+	private Student inputDataOfStudent() {
 		ShowGroupsList mockListOfGroups = Mockito.mock(ShowGroupsList.class);
 
 		InputText mockInputData = Mockito.mock(InputText.class);
@@ -25,18 +38,8 @@ public class InputStudentDataTest {
 		InputGroupID mockGroupID = Mockito.mock(InputGroupID.class);
 		Mockito.when(mockGroupID.inputID()).thenReturn(new Group(1L));
 
-		InputStudentData dataOfStudent = new InputStudentData(mockListOfGroups, mockInputData, mockGroupID);
+		InputStudentData inputStudentData = new InputStudentData(mockListOfGroups, mockInputData, mockGroupID);
 
-		Student result = dataOfStudent.inputData();
-
-		assertNotNull(result);
-		assertEquals("test", result.getFirstName());
-		assertEquals("test", result.getLastrName());
-		assertEquals(1L, result.getGroupID());
-		assertNull(result.getStudentID());
-
-		verify(mockInputData, times(2)).input();
-		verify(mockListOfGroups).groupsList();
-		verify(mockGroupID).inputID();
+		return inputStudentData.inputData();
 	}
 }
